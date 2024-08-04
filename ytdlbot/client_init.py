903364320c7107385722e2a2e69bcd6a758c84a8
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2024-08-04 13:32:02 +0300

    Update client_init.py

--- ytdlbot/client_init.py
+++ ytdlbot/client_init.py
@@ -10,9 +10,19 @@ __author__ = "Benny <benny.think@gmail.com>"
 from pyrogram import Client
 
 from config import APP_HASH, APP_ID, PYRO_WORKERS, TOKEN, IPv6
+import requests
+import random
 
+def get_random_proxy(protocol):
+    url = f'https://api.proxyscrape.com/v2/?request=displayproxies&protocol={protocol}'
+    response = requests.get(url)
+    proxies = response.text.split('\n')
+    return random.choice(proxies).strip()
+
+randproxy = get_random_proxy("socks5")
 
 def create_app(name: str, workers: int = PYRO_WORKERS) -> Client:
+    proxy1 = dict(scheme="socks5", host=randproxy.split(':')[0], port=randproxy.split(':')[1])
     return Client(
         name,
         APP_ID,
@@ -20,6 +30,7 @@ def create_app(name: str, workers: int = PYRO_WORKERS) -> Client:
         bot_token=TOKEN,
         workers=workers,
         ipv6=IPv6,
+        proxy=proxy1,
         # max_concurrent_transmissions=max(1, WORKERS // 2),
         # https://github.com/pyrogram/pyrogram/issues/1225#issuecomment-1446595489
     )
